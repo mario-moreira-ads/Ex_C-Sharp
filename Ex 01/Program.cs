056f67ebe0b64904807ceada327604f95718ac38
Mario Moreira <78828831+mario-moreira-ads@users.noreply.github.com>
2021-04-10 12:25:34 -0300

    Add files via upload

---
+++ Ex 01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Ex_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=2, b=7, c=19, x;
+            x = (c%b)*a;
+
+            Console.WriteLine("O valor de x é: {0}", x);
+        }
+    }
+}
