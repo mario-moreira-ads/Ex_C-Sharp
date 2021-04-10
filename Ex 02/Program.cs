72865ec96ab6659bfbd4f3097b74975567c94237
Mario Moreira <78828831+mario-moreira-ads@users.noreply.github.com>
2021-04-10 12:26:16 -0300

    Add files via upload

---
+++ Ex 02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ex_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x=1, y=2,z=3;
+
+            x=x+y;
+            x=x+z;
+            x=x+3;
+            x=x+x;
+
+            Console.WriteLine("O valor de x é igual a: {0}",x);
+        }
+    }
+}
