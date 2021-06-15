a920b1e7a9919fb9921cb292680483d98439ecd0
Mario Moreira <78828831+mario-moreira-ads@users.noreply.github.com>
2021-06-15 18:38:03 -0300

    Add files via upload

---
+++ array_ex1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Q1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [,] matriz = new int[3,3];
+
+            LeMatriz(matriz);
+
+            Console.WriteLine("Média da primeira linha da matriz: {0}", MediaMatriz(matriz));
+        }
+
+        static int MediaMatriz(int [,] matriz)
+        {
+            int soma =0;
+            int linha = 0;
+
+            
+            for(int j=0; j<matriz.GetLength(1); j++)
+            {
+                soma+=matriz [linha,j];
+            }
+            
+            return soma/matriz.GetLength(1);
+        } 
+
+
+        static void LeMatriz(int [,] matriz)
+        {
+            for (int i=0;i<matriz.GetLength(0);i++)
+            {
+                for(int j=0; j<matriz.GetLength(1); j++)
+                {
+                    Console.Write ("Elemento [{0},{1}]: ", i, j);
+                    matriz [i,j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+        }
+
+    
+                  
+
+       
+    }
+}
+
+
+
+
